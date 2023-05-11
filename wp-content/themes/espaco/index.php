abe6910b8e6b79004e04236cf77a3f5de4c9c50c
alberico fagundes <alberico@fagundes.live>
2023-05-11 15:12:34 -0300

    tema espaco

---
+++ wp-content/themes/espaco/index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css" />
+  <!-- <link href="css/style.css" rel="stylesheet" type="text/css" /> -->
+
+  <link href="<?php echo get_stylesheet_directory_uri() ?>/css/style.css" rel="stylesheet" type="text/css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Espaço Latino</title>
+</head>
+
+<body>
+  <div>
+    <!-- <div class="top-head">
+        <div class="container">
+          <p>43 3324-8300 | espacolatino@live.com</p>
+        </div>
+      </div> -->
+    <section id="top-menu">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#myPage"><img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" alt="logo Espaço Latino" /></a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <!-- <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">Home</a></li>
+                    <li>
+                      <a
+                        href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#coreografia"
+                        >Coreografias</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#aulas"
+                        >Aulas</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#animacao"
+                        >Animação</a
+                      >
+                    </li>
+                    <li><a href="">Blog</a></li>
+                  </ul> -->
+          </div>
+        </div>
+      </nav>
+    </section>
+  </div>
+
+  <!-- <section id="topo" class="banner-bg">
+      <div class="content">
+        <a
+          href="http://espacolatino.com.br/"
+          ><h1>Espaço Latino</h1></a
+        >
+        <p>43 3324-8300 | espacolatino@live.com</p>
+        <ul>
+          <a
+            id="item-1"
+            href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#coreografia"
+            ><li>Coreografias</li></a
+          >
+          <a
+            id="item-2"
+            href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#aulas"
+            ><li>Aulas</li></a
+          >
+          <a
+            id="item-3"
+            href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#animacao"
+            ><li>Animação</li></a
+          >
+        </ul>
+        <img class="dancer-girl" alt="imagem de uma mulher em pose de dança"
+          src="img/imagem-inicial.png"
+        />
+   
+      </div>
+      <div class="page-title">
+        <div class="content">
+            <p>Quem dança é mais feliz</p>
+        </div>
+    </div>
+    </section> -->
+
+  <section id="topo">
+    <div class="content">
+      <a href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/">
+        <h1>Espaço Latino</h1>
+      </a>
+      <p>43 3324-8300 | espacolatino@live.com</p>
+      <ul>
+        <a id="item-1" href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#coreografia">
+          <li>Coreografias</li>
+        </a>
+        <a id="item-2" href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#aulas">
+          <li>Aulas</li>
+        </a>
+        <a id="item-3" href="https://web.archive.org/web/20190429073050/http://espacolatino.com.br/#animacao">
+          <li>Animação</li>
+        </a>
+      </ul>
+      <img src="<?php echo get_stylesheet_directory_uri() ?>/img/imagem-inicial.png" />
+    </div>
+  </section>
+  <section id="page-title">
+    <div class="content">
+      <p>Quem dança é mais feliz</p>
+    </div>
+  </section>
+  <section id="galeria">
+    <div class="content">
+      <p class="titulo">Espaço Latino, qualidade e tradição</p>
+      <p class="conteudo">
+        <br />
+        A Escola foi Fundada em 1988 por Luis Carlos "Mumu",profissional que
+        sempre <br />
+        esteve envolvido com a dança, teatro e a arte em geral. <br />
+        <br />
+        Hoje são <b>31</b> anos de grandes <b>conquistas</b> e
+        <b>realizações</b>!
+      </p>
+      <a href="galeria/index.html"></a>
+    </div>
+  </section>
+
+  <section id="top-de-marcas">
+    <div class="content"></div>
+  </section>
+  <section id="coreografia">
+    <div class="content">
+      <p class="titulo">Coreografias</p>
+      <div class="item-coreografia">
+        Coreografias pensadas e desenvolvidas <br />
+        exclusivamente para <b>casamentos</b>
+        <img src="img/casamento.png" />
+      </div>
+      <div class="item-coreografia item-2">
+        Coreografias pensadas e desenvolvidas <br />
+        exclusivamente para festa de <b>15 anos</b>
+        <img src="img/15anos.png" />
+      </div>
+      <div class="item-coreografia item-3">
+        Eventos em geral
+        <img src="img/mesa-item-3.png" />
+      </div>
+      <a href="coreografia.php">Atendemos diversos tipos de eventos, <b>confira</b></a>
+    </div>
+  </section>
+  <section id="animacao">
+    <div class="content">
+      <img src="img/animacao.png" />
+      <p class="titulo">Animação e interação</p>
+      <p class="conteudo">
+        Possuímos uma equipe para animar sua festa<br />
+        ditamos o ritmo da festa e incentivamos os<br />
+        convidados a participarem
+        <a class="link-srcoll" data-item="#fomulario" href="">entre em contato</a>.
+      </p>
+    </div>
+  </section>
+  <section id="aulas">
+    <div class="content">
+      <p class="titulo">Aulas</p>
+      <p class="conteudo">
+        A Escola trabalha com os seguintes estilos de dança:<br /><br />
+        <b>Dança de salão</b>: sertanejo, forró, samba de gafieira<br />
+        bolero, soltinho, pagode, zouk, salsa, tango, valsa, bugio<br />
+        vanerão, rastapé-merengue e outros... <br /><br />
+        Também trabalhamos com <b>Zumba</b>, <b>Capoeira</b>, <b>Axé</b>,
+        <b>Funk</b>
+      </p>
+      <a href="horarios/index.html">
+        <img src="img/aulas.png" />
+      </a>
+    </div>
+  </section>
+  <section id="fomulario">
+    <div class="content">
+      <div class="formulario grid_6">
+        <form class="form-contato">
+          <fieldset>
+            <div class="clear">
+              <input type="text" id="inputNome" placeholder="Nome" />
+              <input type="text" id="inputEmail" placeholder="E-mail" />
+            </div>
+            <div class="clear">
+              <input type="text" id="inputTelefone" placeholder="Telefone" />
+              <input type="text" id="inputAssunto" placeholder="Assunto" />
+            </div>
+            <div class="clear">
+              <textarea id="inputMensagem" placeholder="Mensagem"></textarea>
+            </div>
+            <div class="clear">
+              <input type="submit" class="inputEnviar" value="Enviar" />
+              <span class="formResposta"></span>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <img src="img/bg-formulario.png" />
+    </div>
+  </section>
+</body>
+
+</html>
